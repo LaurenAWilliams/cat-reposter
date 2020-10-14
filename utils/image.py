59c2d33689b5b861ac40f30394fb82df730f500d
LaurenAWilliams <lwilliams.rhul@gmail.com>
2020-10-14 22:24:49 +0100

    Python doesn't have do whiles hah!

--- utils/image.py
+++ utils/image.py
@@ -7,11 +7,12 @@ HAAR_CASCADE_LOC = 'utils/files/haarcascade_frontalcatface_extended.xml'
 
 
 def _get_image_from_url(url):
-    retry = False
+    retry = True
     while retry:
         try:
             with urllib.request.urlopen(url) as resp:
                 raw_image = numpy.asarray(bytearray(resp.read()), dtype="uint8")
+                retry = False
         except urllib.error.URLError:
             retry = True
     image = cv2.imdecode(raw_image, cv2.IMREAD_COLOR)
